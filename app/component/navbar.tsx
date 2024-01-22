95332ac63c881a3d71bccfeb5a39b2818a0a2ef9
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-01-22 17:25:59 +0500

    Projects

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar(){
       <a href="/" className="mr-5 hover:text-blue-600">Home</a>
       <a href="#About" className="mr-5 hover:text-blue-600">About</a>
       <a href="#" className="mr-5 hover:text-blue-600">Skills</a>
-      <a href="#" className="mr-5 hover:text-blue-600">Projects</a>
+      <a href="#Project" className="mr-5 hover:text-blue-600">Projects</a>
       <a href="#Contact" className="mr-5 hover:text-blue-600">Contact Us</a>
     </nav>
     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image'
 import Hero from './component/hero'
 import Contact from './component/contact'
 import About from './component/about'
+import Project from './component/project'
 
 export default function Home() {
   return (
     <div>
      <Hero/>
+     <Project/>
      <Contact/>  
       <About/>
+      
     </div>
   )
 }
